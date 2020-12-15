26abf89b6deb9fea797e8c80f9f7670297940dae
zhangningle2017 <zhangningle2017@gmail.com>
2020-12-15 14:56:19 +0800

    按照分类生成json

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-//引用文件系统模块
-var fs = require("fs");
-//引用imageinfo模块
-var image = require("imageinfo");
+const path = require('path');
+const fs = require("fs");
+const image = require("imageinfo");
 
 function readFileList(path, filesList) {
     var files = fs.readdirSync(path);
@@ -45,24 +44,81 @@ var getFiles = {
     }
 };
 
+/** 
+ * 文件遍历方法
+ * @param filePath 需要遍历的文件路径 
+ */
+function fileDisplay(filePath) {
+    let dirArr = []
+    let files = fs.readdirSync(filePath);
+    files.forEach(function (filename) {
+        var filedir = path.join(filePath, filename);
+        let stats = fs.statSync(filedir);
+        var isFile = stats.isFile(); //是文件  
+        var isDir = stats.isDirectory(); //是文件夹  
+        if (isFile) {
+            // console.log('是文件：', filedir);
+        }
+        if (isDir) {
+            dirArr.push(filename)
+            // console.log('是文件夹：', filename);
+            fileDisplay(filedir); //递归，如果是文件夹，就继续遍历该文件夹下面的文件  
+        }
+    });
+    return dirArr;
+}
+let files = fileDisplay('./imgs/')
+console.log('files', files)
+
+
 //获取文件夹下的所有图片
 // let imgs1 = getFiles.getImageFiles("./imgs/");
 //获取文件夹下的所有文件
 let imgs2 = getFiles.getFileList("./imgs/");
 // console.log(imgs1, imgs2)
-let str = ''
-imgs2.forEach((e, i, arr) => {
-    let src = e.path + e.filename;
-    str += src + (i == arr.length - 1 ? '' : ',')
+let joinDataArr = [];
+
+
+files.forEach(el => {
+    let joinDataObj = {
+        strArr: [],
+        title: ''
+    }
+    let str = ''
+    joinDataObj.title = el;
+    imgs2.forEach((e, i, arr) => {
+        let src = e.path + e.filename;
+        console.log(src)
+        if (src.indexOf(joinDataObj.title) != -1) {
+            str += src + (i == arr.length - 1 ? '' : ',')
+        }
+    })
+    joinDataObj.strArr = str.split(',').filter(e => e !== '')
+    joinDataArr.push(joinDataObj)
 })
-let arr = str.split(',')
-console.log(arr)
-fs.writeFile('./js/json.js', 'let arr = ' + JSON.stringify(arr), err => {
+// console.log(joinDataArr)
+
+
+let writeStr = 'const arr = ' + JSON.stringify(joinDataArr)
+fs.writeFile('./js/json.js', writeStr, err => {
     if (err) {
         console.error(err)
         return
     } else {
-        console.log('写入成功', str)
+        console.log('写入成功', writeStr)
     }
-    //文件写入成功。
 })
+
+/**
+ * 最后组成数据格式：
+ [{
+    title: '类型1',
+    arr: ['./imgs/xx.png', './imgs/xx.png']
+    },
+    {
+    title: '类型1',
+    arr: ['./imgs/xx.png', './imgs/xx.png']
+    }
+ ]
+ * 
+ */
--- js/json.js
+++ js/json.js
@@ -1,1 +1,13 @@
-let arr = ["./imgs/loading.gif","./imgs/钢笔分类A/1280-520587640-red-panda.jpg","./imgs/钢笔分类A/1280-601902040-red-panda-napping.jpg","./imgs/钢笔分类A/f0at2n_web.webp","./imgs/钢笔分类A/OIP.jpg","./imgs/钢笔分类A/penA1 - 副本 (2).jpg","./imgs/钢笔分类A/penA1 - 副本 (3).jpg","./imgs/钢笔分类A/penA1 - 副本.jpg","./imgs/钢笔分类A/penA1.jpg","./imgs/钢笔分类A/penB1 - 副本 (12).jpg","./imgs/钢笔分类A/penB1 - 副本 (6).jpg","./imgs/钢笔分类B/download.jpg","./imgs/钢笔分类B/OIP.jpg","./imgs/钢笔分类B/penB1 - 副本 (22) - 副本.jpg","./imgs/钢笔分类B/penB1 - 副本 - 副本.jpg"]
+const arr = [{
+    "strArr": ["./imgs/钢笔分类A/1280-520587640-red-panda.jpg", "./imgs/钢笔分类A/1280-601902040-red-panda-napping.jpg", "./imgs/钢笔分类A/f0at2n_web.webp", "./imgs/钢笔分类A/OIP.jpg", "./imgs/钢笔分类A/penA1 - 副本 (2).jpg", "./imgs/钢笔分类A/penA1 - 副本 (3).jpg", "./imgs/钢笔分类A/penA1 - 副本.jpg", "./imgs/钢笔分类A/penA1.jpg", "./imgs/钢笔分类A/penB1 - 副本 (12).jpg", "./imgs/钢笔分类A/penB1 - 副本 (6).jpg"],
+    "title": "钢笔分类A"
+}, {
+    "strArr": ["./imgs/钢笔分类B/download.jpg", "./imgs/钢笔分类B/OIP.jpg", "./imgs/钢笔分类B/penB1 - 副本 (22) - 副本.jpg", "./imgs/钢笔分类B/penB1 - 副本 - 副本.jpg"],
+    "title": "钢笔分类B"
+}, {
+    "strArr": ["./imgs/钢笔分类C/download.jpg", "./imgs/钢笔分类C/OIP.jpg", "./imgs/钢笔分类C/penB1 - 副本 (22) - 副本.jpg", "./imgs/钢笔分类C/penB1 - 副本 - 副本.jpg"],
+    "title": "钢笔分类C"
+}, {
+    "strArr": ["./imgs/钢笔分类D/1280-520587640-red-panda.jpg", "./imgs/钢笔分类D/1280-601902040-red-panda-napping.jpg", "./imgs/钢笔分类D/f0at2n_web.webp", "./imgs/钢笔分类D/OIP.jpg", "./imgs/钢笔分类D/penA1 - 副本 (2).jpg", "./imgs/钢笔分类D/penA1 - 副本 (3).jpg", "./imgs/钢笔分类D/penA1 - 副本.jpg", "./imgs/钢笔分类D/penA1.jpg", "./imgs/钢笔分类D/penB1 - 副本 (12).jpg", "./imgs/钢笔分类D/penB1 - 副本 (6).jpg"],
+    "title": "钢笔分类D"
+}]
